c956ab28a12befa08cd61d0ac5e5395d6c805744
padi-dev-namtt <namtt@paditech.com>
2024-03-18 17:48:27 +0700

    update

--- src/utils/removeCookie.ts
+++ src/utils/removeCookie.ts
@@ -4,4 +4,8 @@ export const removeCookie = () => {
   const cookies = new Cookies();
 
   cookies.remove("user_id", { path: "/" });
+  cookies.remove("user_name", { path: "/" });
+  cookies.remove("full_name", { path: "/" });
+  cookies.remove("email", { path: "/" });
+  cookies.remove("avatar_path", { path: "/" });
 };
